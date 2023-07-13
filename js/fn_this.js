4870401c5022e6a0d0d7b39f71d023d4b1e77d01
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-14 00:16:15 +0900

    javascript try this

---
+++ js/fn_this.js
@@ -0,0 +1,28 @@
+const expressFn = function(name) {
+  this.name = name;
+  console.log(this, new.target, this.name, name);
+}
+
+const arrowFn = (name) => {
+  this.name = name;
+  console.log(this, new.target, this.name, name);
+}
+
+expressFn('expfn');
+arrowFn('afn');
+
+console.log('-'.repeat(10));
+
+const Dog = function(name) {
+  console.log(this, new.target, this instanceof Dog, this.name);
+  this.name = name;
+  this.bark = function() {
+    console.log('bark= ', new.target, this.name, name);
+  }
+  this.bark2 = () => {
+    console.log('bark2= ', new.target, this.name, name);
+  }
+};
+
+const dog = Dog('Doggy');
+const lucy = new Dog('Lucy');
---
+++ js/tco.js
@@ -0,0 +1,41 @@
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n-1);
+}
+
+function factorialTCO(n, acc = 1) {
+  if (n === 0) return acc;
+  return factorialTCO(n-1, n * acc);
+}
+
+for(let i = 0; i <= 10; i++) {
+  console.log(i, factorial(i));
+}
+
+console.log('-'.repeat(10));
+
+for(let i = 0; i <= 10; i++) {
+  console.log(i, factorialTCO(i));
+}
+
+console.log('-'.repeat(10));
+
+let runCnt = 0;
+
+function fibonacci(n) {
+  runCnt += 1;
+  if( n < 2 ) return n;
+  return fibonacci(n-2) + fibonacci(n-1);
+}
+
+const fibonacciTCO = (n, pp = 0, p = 1) => n === 0 ? pp : fibonacciTCO(n-1, p, pp + p);
+
+for(let i = 0; i <= 10; i++) {
+  console.log(i, fibonacci(i));
+}
+
+console.log('-'.repeat(10));
+
+for(let i = 0; i <= 10; i++) {
+  console.log(i, fibonacciTCO(i));
+}ㄴ
