c26c24d00aeda05b1133a64a45e5fcd204a835f4
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-13 14:27:24 +0900

    closure code refactoring

--- js/closure.js
+++ js/closure.js
@@ -6,48 +6,42 @@ const MENU = {
 
 function bill() {
   const orderedItems = [];
+  const billFormat = () => {
+    console.log("=".repeat(21));
+  };
   const makeRightString = (s, len = 9) => {
     s = s.toLocaleString();
     const t = ' '.repeat(len) + s + '원';
     return t.substring(t.length - len);
-  }
-
+  };
+  const pritTaxes = (tax) => {
+    console.log("부가세액: ", makeRightString(tax));
+    console.log(" - ".repeat(7));
+  };
+  const printTotalPrices = (prices) => {
+    console.log("주문합계: ", makeRightString(prices));
+  }; 
+  const printTotalTaxes = (taxes) => {
+    console.log("세액합계: ", makeRightString(taxes));
+  }; 
   return {
     order(thing) {
       orderedItems.push(thing);
     },
     printBill() {
+      billFormat();
       let prices = 0, taxes = 0;
-  
-      // bill 시작
-      console.log("=".repeat(21));
-
-      // 주문 내역 출력
       for (item of orderedItems) {
         let price = MENU[item].price;
-        let tax = Math.round( (MENU[item].price/1.1) * 0.1);
-
+        let tax = MENU[item].taxFree === 1 ? 0 : Math.round((MENU[item].price / 1.1) * 0.1);
         console.log(item);
         console.log("공급가액: ", makeRightString(price));
         prices += price;
-
-        if(MENU[item].taxFree) {
-          console.log("부가세액: ", makeRightString(0));
-          console.log(" - ".repeat(7));
-        }
-        else {
-          console.log("부가세액: ", makeRightString(tax) )
-          console.log(" - ".repeat(7));
-          taxes += tax;
-        }
+        MENU[item].taxFree === 1 ? pritTaxes(0) : pritTaxes(tax), taxes += tax;
       }
-
-      // 최종 금액 출력
-      console.log("주문합계: ", makeRightString(prices));
-      console.log("세액합계: ", makeRightString(taxes));
-
-      // bill 끝
-      console.log("=".repeat(21));
+      printTotalPrices();
+      printTotalTaxes();
+      billFormat();
     }
   };
 }
@@ -58,4 +52,7 @@ table1.order('짬뽕');
 table1.printBill();
 
 table1.order('탕슉');
+table1.printBill();
+
+table1.order('짬뽕');
 table1.printBill();
