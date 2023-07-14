4bfdf053bffaec0a8f7b2ec3ad5ff02641fb2a8d
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-14 12:55:56 +0900

    cloure code refactoring

--- js/closure.js
+++ js/closure.js
@@ -4,48 +4,47 @@ const MENU = {
   탕슉: { price: 25000, taxFree: 1 },
 };
 
+// 보조 함수
+const printFormat = (len = 21) => {
+  console.log("=".repeat(len));
+};
+const makeRightString = (s, len = 9) => {
+  const t = ' '.repeat(len) + s.toLocaleString() + '원';
+  return t.substring(t.length - len);
+};
+const printOrderAndPriceAndTax = (order, price, tax) => {
+  console.log(order);
+  console.log("공급가액: ", makeRightString(price));
+  console.log("부가세액: ", makeRightString(tax));
+};
+const printTotalPricesAndTaxes = (prices, taxes) => {
+  console.log("주문합계: ", makeRightString(prices));
+  console.log("세액합계: ", makeRightString(taxes));
+};
+
 function bill() {
   const orders = [];
-  const printFormat = (len = 21) => {
-    console.log("=".repeat(len));
-  };
-  const makeRightString = (s, len = 9) => {
-    const t = ' '.repeat(len) + s.toLocaleString() + '원';
-    return t.substring(t.length - len);
-  };
-  const printOrderAndPriceAndTax = (order, price, tax) => {
-    console.log(order);
-    console.log("공급가액: ", makeRightString(price));
-    console.log("부가세액: ", makeRightString(tax));
-  };
-  const printTotalPricesAndTaxes = (prices, taxes) => {
-    console.log("주문합계: ", makeRightString(prices));
-    console.log("세액합계: ", makeRightString(taxes));
-  };
 
-  const order= (thing) => {
-    orders.push(thing);
-  };
-
-  const printBill = () => {
-    let prices = 0, taxes = 0;
-    printFormat();
-    for (const order of orders) {
-      let price = MENU[order].price;
-      let tax = MENU[order].taxFree === 1 ? 0 : Math.round( (MENU[order].price/1.1) * 0.1);
-      printOrderAndPriceAndTax(order, price, tax)
-      console.log(" - ".repeat(7));
-      prices += price;
-      taxes += tax;
-    }
-    printTotalPricesAndTaxes(prices, taxes);
-    printFormat();
-  };
-
+  // 클로저
   return {
-    order,
-    printBill,
-  }
+    order(thing) {
+      orders.push(thing);
+    },
+    printBill() {
+      let prices = 0, taxes = 0;
+      printFormat();
+      for (const order of orders) {
+        let price = MENU[order].price;
+        let tax = MENU[order].taxFree === 1 ? 0 : Math.round( (MENU[order].price/1.1) * 0.1);
+        printOrderAndPriceAndTax(order, price, tax)
+        console.log(" - ".repeat(7));
+        prices += price;
+        taxes += tax;
+      }
+      printTotalPricesAndTaxes(prices, taxes);
+      printFormat();
+    }
+  };
 }
 
 const table1 = bill();
@@ -57,4 +56,5 @@ table1.order('탕슉');
 table1.printBill();
 
 table1.order('탕슉');
+table1.order('짬뽕');
 table1.printBill();
