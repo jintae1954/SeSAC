4954b5db88b2f91b600b02688fd2759446a592ea
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-16 01:07:57 +0900

    javascript try this

--- jsTryThis/reduce.js
+++ jsTryThis/reduce.js
@@ -1,10 +1,12 @@
-const reduce = (src, fn, accumulation = 0) => {
-  
-  console.log("src: ", src);
-  console.log("fn: ", fn);
-  console.log("accumulation: ", accumulation);
-
-  
+const reduce = (arr, fn, initValue = 0) => {
+  let idx = 0;
+  let acc = initValue ?? (idx++, arr[0]);
+  for(let i = 0; i < arr.length; i++) {
+    acc = fn(acc, arr[i]);
+  }
+  return acc;
 };
 
 console.log(reduce([1,2,3], (s, a) => s + a, 0));
+console.log(reduce([1,2,3], (s, a) => s + a));
+console.log(reduce([2,2,2], (s, a) => s * a, 1));
