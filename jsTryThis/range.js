3fbf192bbbfe028f859f6b7d353cd86381d098fb
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-19 20:41:53 +0900

    javascript try this

--- jsTryThis/range.js
+++ jsTryThis/range.js
@@ -1,28 +1,23 @@
 /**
  * Part 1. 다음과 같은 정수 배열을 생성하는 range 함수를 구현하시오.
  */
-function range(...args) {
-  let arr = [];
+const range = (...args) => {
+  console.log("args\n", args);
 
-  const initStart = 1;
-  const initEnd =
-  console.log(arguments);
-  console.log(args[0]);
-  console.log(args[1]);
-  console.log(args[2]);
+  let start = args[0];
+  let end = args[1];
+  let step = args[2];
+  
+
+  console.log("ret\n", ret);
+  return ret;
+};
 
 
-  console.log(arr);
-
-  return arr;
-}
-
-// [1,2,3,4,5,7,8,9,10]
-range(1, 10, 1);
-
-// range(1, 10, 2); // [1,3,5,7,9]
-// range(1, 10); // [1,2,3,4,5,7,8,9,10]
-// range(10, 1); // [10,9,8,7,6,5,4,3,2,1]
+range(1, 10, 1); // [1,2,3,4,5,7,8,9,10]
+range(1, 10, 2); // [1,3,5,7,9]
+range(1, 10); // [1,2,3,4,5,7,8,9,10]
+range(10, 1); // [10,9,8,7,6,5,4,3,2,1]
 // range(10, 1, -2); // [10,8,6,4,2]
 
 // [1,2,3,4,5]
