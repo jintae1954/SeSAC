84eadb661d77e41746dd7f0017a11afb83f4d136
jintae1954 <qkrwlsxo1954@gmail.com>
2023-07-19 20:43:26 +0900

    javascript try this

---
+++ jsTryThis/for_each_map.js
@@ -0,0 +1,14 @@
+/**
+ * Part 1. 주어진 정수 배열에서 각 요소를 제곱한 값들과 제곱근 값들을 반환하는 함수를
+ * for-of, forEach, map을 사용하여 각각 작성하시오.
+ */
+
+const arr = [1, 4, 9];
+
+const powSqrtByForOf = (arr) => {
+  
+}
+
+console.log(powSqrtByForOf(arr));
+console.log(powSqrtByForEach(arr));
+console.log(powSqrtByMap(arr));
