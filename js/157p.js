ad741677549de532c8aff8b941d35459fb9755c7
jintae1954 <qkrwlsxo1954@gmail.com>
2023-08-21 23:05:02 +0900

    js try this

--- js/157p.js
+++ js/157p.js
@@ -1,6 +1,5 @@
 /**
- * Part 1. Collection 클래스를 상속 받아 List 메소드들과
- *  클래스 메소드 arrayToList, listToArray를 보유한 ArrayList 클래스를 구현하시오.
+ * Part 1. 이전에 iterator로 작성한 Stack과 Queue의 iterator를 generator 함수로 작성하시오.
  */
 
 class Collection {
