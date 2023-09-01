3781560ccf01361621cdf4f3f77146f5999960c3
jintae1954 <qkrwlsxo1954@gmail.com>
2023-09-01 13:09:20 +0900

    ts Dependency Injection

--- ts/dogcat.ts
+++ ts/dogcat.ts
@@ -1,18 +1,52 @@
 class Animal {
-  name;
+  #name;
   constructor(name: string) {
-    this.name = name;
+    this.#name = name;
+  }
+
+  bark() {}
+  getName() {
+    return this.#name;
   }
 }
 
-class Dog extends Animal {
+class BaseAnimal extends Animal {
+  private ani: Animal;
+
+  constructor(aniOrName: string | Animal) {
+    if (typeof aniOrName === 'string') {
+      super(aniOrName);
+      this.ani = new Animal(aniOrName);
+    } else {
+      super(aniOrName.getName());
+      this.ani = aniOrName;
+    }
+  }
+
+  bark(): void {
+    super.bark();
+    this.ani.bark();
+  }
+}
+
+class Dog extends BaseAnimal {
   bark() {
+    super.bark();
     console.log('멍멍');
   }
 }
 
-class Cat extends Animal {
+class Cat extends BaseAnimal {
   bark() {
-    console.log('야옹');
+    super.bark();
+    console.log('야옹~');
   }
 }
+
+const x = new Dog(new Cat('개냥이'));
+const y = new Cat(new Dog('개냥이'));
+const z = new Dog('개');
+
+x.bark();
+y.bark();
+z.bark();
